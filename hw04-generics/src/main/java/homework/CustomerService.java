48fa264cf5acd47cc728d75455730fa2f0d558a7
RWEC\akrecev <akretsev@gmail.com>
2024-01-22 11:55:46 +0300

    extract private method on CustomerService

--- hw04-generics/src/main/java/homework/CustomerService.java
+++ hw04-generics/src/main/java/homework/CustomerService.java
@@ -10,29 +10,23 @@ public class CustomerService {
     private final TreeMap<Customer, String> customerMap = new TreeMap<>(Comparator.comparing(Customer::getScores));
 
     public Map.Entry<Customer, String> getSmallest() {
-
-        return customerMap.firstKey() == null
-                ? null
-                : new AbstractMap.SimpleEntry<>(
-                        new Customer(
-                                customerMap.firstKey().getId(),
-                                customerMap.firstKey().getName(),
-                                customerMap.firstKey().getScores()),
-                        customerMap.firstEntry().getValue());
+        return getEntry(customerMap.firstKey(), customerMap.firstEntry().getValue());
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return customerMap.higherKey(customer) == null
-                ? null
-                : new AbstractMap.SimpleEntry<>(
-                        new Customer(
-                                customerMap.higherKey(customer).getId(),
-                                customerMap.higherKey(customer).getName(),
-                                customerMap.higherKey(customer).getScores()),
-                        customerMap.ceilingEntry(customer).getValue());
+        return getEntry(
+                customerMap.higherKey(customer),
+                customerMap.ceilingEntry(customer).getValue());
     }
 
     public void add(Customer customer, String data) {
         customerMap.put(customer, data);
+    }
+
+    private Map.Entry<Customer, String> getEntry(Customer customer, String value) {
+        return customer == null
+                ? null
+                : new AbstractMap.SimpleEntry<>(
+                        new Customer(customer.getId(), customer.getName(), customer.getScores()), value);
     }
 }
