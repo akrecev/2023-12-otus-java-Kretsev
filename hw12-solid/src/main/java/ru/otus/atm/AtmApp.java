2b4e1be1c78505dc69c629029072dc477535e477
RWEC\akrecev <akretsev@gmail.com>
2024-02-21 15:55:43 +0300

    add basic logic

--- hw12-solid/src/main/java/ru/otus/atm/AtmApp.java
+++ hw12-solid/src/main/java/ru/otus/atm/AtmApp.java
@@ -1,11 +1,29 @@
 package ru.otus.atm;
 
+import java.util.EnumMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import ru.otus.atm.model.Banknote;
+import ru.otus.atm.repository.impl.SimpleBanknoteStorageImpl;
+import ru.otus.atm.rest.AtmController;
+import ru.otus.atm.service.impl.SimpleAtmServiceImpl;
 
 @Slf4j
 public class AtmApp {
     public static void main(String[] args) {
         log.debug("Starting application...");
 
+        AtmController atmController = new AtmController(new SimpleAtmServiceImpl(new SimpleBanknoteStorageImpl()));
+        Map<Banknote, Integer> banknotes = new EnumMap<>(Banknote.class);
+        banknotes.put(Banknote.BN_1000, 2);
+        banknotes.put(Banknote.BN_100, 5);
+
+        log.debug(String.valueOf(atmController.show()));
+
+        log.debug(String.valueOf(atmController.deposit(banknotes)));
+
+        atmController.get(1300);
+
+        log.debug(String.valueOf(atmController.show()));
     }
 }
---
+++ hw12-solid/src/main/java/ru/otus/atm/exception/NotEnoughBanknotesException.java
@@ -0,0 +1,6 @@
+package ru.otus.atm.exception;
+
+import lombok.experimental.StandardException;
+
+@StandardException
+public class NotEnoughBanknotesException extends RuntimeException {}
