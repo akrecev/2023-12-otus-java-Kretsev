52a9cf7e14d1061efe7db8518d17fbaa0ad2b2bc
RWEC\akrecev <akretsev@gmail.com>
2024-02-21 11:37:22 +0300

    initial hw12-solid

--- hw12-solid/build.gradle.kts
+++ hw12-solid/build.gradle.kts
@@ -1,4 +1,7 @@
 dependencies {
+    implementation("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    implementation ("org.assertj:assertj-core")
+
+    implementation("ch.qos.logback:logback-classic")
 }
-
-
