152f0e675be9dc5fcf83e28eea3c93b8d811fecd
akrecev <akrecev@gmail.com>
2024-03-11 21:04:27 +0300

    todo: 1. Add fields field11 - field13 (field13 - ObjectForMessage)

--- hw15-structuralPatterns/homework/build.gradle.kts
+++ hw15-structuralPatterns/homework/build.gradle.kts
@@ -1,5 +1,14 @@
+plugins {
+    id ("io.freefair.lombok")
+}
 
 dependencies {
+    implementation("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    implementation("ch.qos.logback:logback-classic")
+
+    testImplementation("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
     testImplementation ("org.junit.jupiter:junit-jupiter-api")
     testImplementation ("org.junit.jupiter:junit-jupiter-engine")
     testImplementation ("org.junit.jupiter:junit-jupiter-params")
