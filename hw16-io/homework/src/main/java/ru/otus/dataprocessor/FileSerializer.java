708cbea120c9483348964259f1ca5918fccbf89b
Andrey Kretsev <akrecev@gmail.com>
2024-04-19 21:51:53 +0300

    add serializer

--- hw16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,13 +1,22 @@
 package ru.otus.dataprocessor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
+    private final ObjectMapper mapper;
+    private final String fileName;
 
-    public FileSerializer(String fileName) {}
+    public FileSerializer(String fileName) {
+        this.mapper = JsonMapper.builder().build();
+        this.fileName = fileName;
+    }
 
     @Override
-    public void serialize(Map<String, Double> data) {
-        // формирует результирующий json и сохраняет его в файл
+    public void serialize(Map<String, Double> data) throws IOException {
+        mapper.writeValue(new File(fileName), data);
     }
 }
