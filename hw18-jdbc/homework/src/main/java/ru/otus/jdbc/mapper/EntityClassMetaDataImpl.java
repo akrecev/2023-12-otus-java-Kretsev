9826b275a54b7a6b42dfad72f0bd6e390e387dc3
RWEC\akrecev <akretsev@gmail.com>
2024-05-15 17:02:15 +0300

    add EntityClassMetaDataImpl

---
+++ hw18-jdbc/homework/src/main/java/ru/otus/jdbc/mapper/EntityClassMetaDataImpl.java
@@ -0,0 +1,53 @@
+package ru.otus.jdbc.mapper;
+
+import ru.otus.jdbc.annotations.Id;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.function.Predicate.not;
+
+public class EntityClassMetaDataImpl<T> implements EntityClassMetaData<T> {
+    private final Class<T> clazz;
+
+    public EntityClassMetaDataImpl(Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    @Override
+    public String getName() {
+        return clazz.getSimpleName();
+    }
+
+    @Override
+    public Constructor<T> getConstructor() {
+        try {
+            return clazz.getConstructor(clazz.getFields().getClass());
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException("Exception getting constructor", e);
+        }
+    }
+
+    @Override
+    public Field getIdField() {
+        return Arrays.stream(clazz.getDeclaredFields())
+                .filter(f -> f.isAnnotationPresent(Id.class))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    @Override
+    public List<Field> getAllFields() {
+        return Arrays.stream(clazz.getDeclaredFields()).toList();
+    }
+
+    @Override
+    public List<Field> getFieldsWithoutId() {
+        return Arrays.stream(clazz.getDeclaredFields())
+                .filter(not(f -> f.isAnnotationPresent(Id.class)))
+                .collect(Collectors.toList());
+    }
+}
